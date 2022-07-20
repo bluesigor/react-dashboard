5916320e6279be34a6951039a4631dfa28b6af2d
bluesigor <87902135+bluesigor@users.noreply.github.com>
2022-07-20 12:10:43 +0300

    feat:formik added

--- src/contexts/LoginContext.js
+++ src/contexts/LoginContext.js
@@ -1,46 +1,35 @@
-import { useFormik } from 'formik'
 import { createContext, useContext, useState } from 'react'
+import * as Yup from 'yup'
 
 export const LoginContext = createContext()
 
 export const ContextProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false)
   const [showPassword, setShowPassword] = useState(false)
+  const togglePassword = () => {
+    setShowPassword((prev) => !prev)
+  }
   const initialValues = {
     email: '',
     password: '',
   }
-  const validate = (values) => {
-    let errors = {}
-    if (!values.password) {
-      errors.password = 'Required'
-    }
-    if (!values.email) {
-      errors.email = 'Requiered'
-    } else if (
-      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
-    ) {
-      errors.email = 'Invalid email'
-    }
-    return errors
-  }
+  const validationSchema = Yup.object({
+    email: Yup.string().email('Invalid email format').required('Required'),
+    password: Yup.string().required('Required'),
+  })
   const onSubmit = (values) => {
     if (values.email === 'jones@gmail.com' && values.password === 'jones111') {
       setIsLoggedIn(true)
     }
-  }
-  const formik = useFormik({
-    initialValues,
-    onSubmit,
-    validate,
-  })
-  const togglePassword = () => {
-    setShowPassword((prev) => !prev)
+    values.email = ''
+    values.password = ''
   }
   return (
     <LoginContext.Provider
       value={{
-        formik,
+        onSubmit,
+        validationSchema,
+        initialValues,
         isLoggedIn,
         setIsLoggedIn,
         showPassword,
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,74 +1,86 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
+
+import { useLoginContext } from '../../contexts/LoginContext'
+import { Form, Formik, Field, ErrorMessage } from 'formik'
 import './style.scss'
 import logo from './images/logo.svg'
 import showlogo from './images/active.svg'
-import { useLoginContext } from '../../contexts/LoginContext'
-const Login = () => {
-  const { formik, showPassword, togglePassword } = useLoginContext()
+
+function Login() {
+  const {
+    initialValues,
+    validationSchema,
+    onSubmit,
+    showPassword,
+    togglePassword,
+  } = useLoginContext()
 
   return (
     <div className="outer">
-      <form onSubmit={formik.handleSubmit} className="outer-inner">
-        <section className="outer-inner-logo">
-          <a href="../index.html">
-            <img src={logo} alt="logo" />
-            <h4> Dashboard Kit </h4>{' '}
-          </a>{' '}
-        </section>{' '}
-        <section className="outer-title">
-          <h3> Log In to Dashboard Kit </h3>{' '}
-          <p> Enter your email and password below </p>{' '}
-        </section>{' '}
-        <section className="outer-input">
-          <div className="outer-input-email">
-            <label htmlFor="email"> EMAIL </label>{' '}
-            <input
-              onChange={formik.handleChange}
-              type="text"
-              value={formik.values.email}
-              id="email"
-              placeholder="Email address"
-              name="email"
-            />
-            {formik.errors.email ? (
-              <p className="outer-wrong">{formik.errors.email}</p>
-            ) : null}
-          </div>{' '}
-          <div className="outer-input-password">
-            <div className="label-block">
-              <label id="password-label" htmlFor="password">
-                PASSWORD{' '}
-              </label>{' '}
-              <label id="forgot-label" htmlFor="password">
-                Forgot password ?
-              </label>{' '}
+      <Formik
+        className=""
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={onSubmit}
+      >
+        <Form className="outer-inner">
+          <section className="outer-inner-logo">
+            <a href="../index.html">
+              <img src={logo} alt="logo" />
+              <h4> Dashboard Kit </h4>{' '}
+            </a>{' '}
+          </section>{' '}
+          <section className="outer-title">
+            <h3> Log In to Dashboard Kit </h3>{' '}
+            <p> Enter your email and password below </p>{' '}
+          </section>{' '}
+          <section className="outer-input">
+            <div className="outer-input-email">
+              <label htmlFor="email"> EMAIL </label>{' '}
+              <Field
+                type="text"
+                id="email"
+                placeholder="Email address"
+                name="email"
+              />
+              <p className="outer-input-email-wrong">
+                <ErrorMessage name="email" />
+              </p>
             </div>{' '}
-            <input
-              onChange={formik.handleChange}
-              type={showPassword ? 'text' : 'password'}
-              value={formik.values.password}
-              id="password"
-              placeholder="Password"
-              name="password"
-            />
-            {formik.errors.password ? (
-              <p className="outer-wrong">{formik.errors.password}</p>
-            ) : null}
-            <div className="outer-input-password-show__logo">
-              <button onClick={togglePassword} type="button">
-                <img src={showlogo} alt="showlogo" />
-              </button>{' '}
+            <div className="outer-input-password">
+              <div className="outer-input-password-label-block">
+                <label id="password-label" htmlFor="password">
+                  PASSWORD{' '}
+                </label>{' '}
+                <label id="forgot-label" htmlFor="password">
+                  Forgot password ?
+                </label>{' '}
+              </div>{' '}
+              <Field
+                type={showPassword ? 'text' : 'password'}
+                id="password"
+                placeholder="Password"
+                name="password"
+              />
+              <p className="outer-input-password-wrong">
+                <ErrorMessage name="password" />
+              </p>
+              <div className="outer-input-password-show__logo">
+                <button onClick={togglePassword} type="button">
+                  <img src={showlogo} alt="showlogo" />
+                </button>{' '}
+              </div>{' '}
             </div>{' '}
-          </div>{' '}
-        </section>{' '}
-        <button type="submit" className="outer-login-btn">
-          Submit{' '}
-        </button>{' '}
-        <h4 className="outer-signup">
-          Don 't have an account? <a href="#"> Sign up </a>{' '}
-        </h4>{' '}
-      </form>{' '}
+          </section>{' '}
+          <button type="submit" className="outer-login-btn">
+            Submit{' '}
+          </button>{' '}
+          <h4 className="outer-signup">
+            Don 't have an account? <a href="#"> Sign up </a>{' '}
+          </h4>{' '}
+        </Form>
+      </Formik>
     </div>
   )
 }
